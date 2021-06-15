e594b2bb8cb7264bdf5215997fb46a901f187177
laragalazdg <galazlv.nqn@gmail.com>
2021-06-15 19:19:54 -0300

    formularios realizados

--- resources/views/formregistro.blade.php
+++ resources/views/formregistro.blade.php
@@ -50,5 +50,37 @@
             </div>
         </div>
     </div>
+
+    <div class="container-fluid px-1 py-5 mx-auto">
+        <div class="row d-flex justify-content-center">
+            <div class="col-xl-7 col-lg-8 col-md-9 col-11">
+                <div class="card">
+                    {{-- <h5 class="text-center mb-1">Registro de Usuario</h5> --}}
+                    <form class="form-card">
+                        <div class="row justify-content-between text-left">
+                            <div class="form-group col-12 flex-column d-flex py-3"> 
+                                <label class="form-control-label px-3 py-2">Email</label> 
+                                <input class="border-0 cell" type="text" id="email" name="email" placeholder="Ingrese su email"> 
+                            </div>
+                        </div>
+                        <div class="row justify-content-between text-left">
+                            <div class="form-group col-12 flex-column d-flex py-3"> 
+                                <label class="form-control-label px-3 py-2">Contraseña</label> 
+                                <input class="border-0 cell" type="text" id="password" name="password" placeholder="Ingrese su contraseña"> 
+                            </div>
+                        </div>
+                        <div class="row justify-content-between text-left py-3">
+                            <p><a href="">¿Olvido su contraseña?</a></p>
+                        </div>
+                        <div class="row justify-content-center text-center py-4">
+                            <div class="form-group col-sm-6">
+                                <button id="boton" name="boton" type="submit" class="btn-block w-100 p-1 rounded-2">Ingresar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
     
     @endsection
