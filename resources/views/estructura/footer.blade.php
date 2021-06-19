d6b667b795f623f9d1fadf5ff6ac2f2b9eac896f
Arian <arian_eb@hotmail.com>
2021-06-19 19:24:35 -0300

    Cambio de medida footer

--- resources/views/estructura/footer.blade.php
+++ resources/views/estructura/footer.blade.php
@@ -1,18 +1,18 @@
 <footer class=mt-auto> {{-- Inicio pie --}}
     <div class="d-flex justify-content-md-center p-2 text-light" id=pie>
         <div class="container row">
-            <div class="col text-md-start">
+            <div class="col text-sm-start">
                 <h5>Contactanos</h5>
                 <p>
                     Email: ejemplo@example.com<br>
                     Teléfono: 0299 123 4567
                 </p>
             </div>
-            <div class="col text-center">
+            <div class="col text-sm-center">
                 <img src="{{ asset('img/isologo_uncoma_blanco_chico.png') }}" alt="Logo UNComa"
                     style="max-height: 100px">
             </div>
-            <div class="col text-md-end">
+            <div class="col text-sm-end">
                 <h5>Universidad Nacional del Comahue</h5>
                 <p>
                     Buenos Aires 1400<br>
