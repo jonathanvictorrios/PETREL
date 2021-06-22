a9815f9fdc918db7df81b52a0e5aa205a0d241ba
laragalazdg <galazlv.nqn@gmail.com>
2021-06-22 11:24:33 -0300

    modificaciones en formularios actuales

--- resources/views/solicitud/show.blade.php
+++ resources/views/solicitud/show.blade.php
@@ -12,40 +12,78 @@
 {{-- inicion mostrar solicitud --}}
 <div class="container-fluid p-1 mx-auto">
     <div class="row d-flex justify-content-center">
-        <div class="col-xl-7 col-lg-8 col-md-9 col-12">
+        <div class="col-xl-6 col-lg-6 col-md-12 col-12">
 
             <div class="card card-form bg-light">
-                <div class="tittle card-header p-1 bg-light cell mb-3">
-                    <h2 class="text-center fw-bold">Detalles de Solicitud {{$solicitud->idSolicitud}}</h2>
+                <div class="row tittle card-header p-2 bg-light">
+                    <h2 class="col text-center fw-bold">Detalles de Solicitud</h2>
                 </div>
-                {{-- fecha --}}
-                <div class="row justify-content-between text-left ">
-                    <p class=" "><span class="text-secondary fs-5">Fecha de Inicio: </span> {{$solicitud->FechaUltimoEstado}}</p> 
-                </div>
-                 {{-- estado --}}
-                <div class="row justify-content-between text-left ">
-                    <p class=" "><span class="text-secondary fs-5">Estado: </span> en progreso{{-- ACÀ DEBE IR ESTADO DE ESTUDIANTE{{$solicitud->UltimoEstado}}</p>  --}}
+                {{-- fecha y estado --}}
+                <div class="row justify-content-between text-left cell">
+                    <div class="form-group col col-sm-6 flex-column d-flex py-3">
+                            <label class="form-control-label px-3 py-2">Fecha de Inicio</label>
+                            <input type="text" id="fecha" class="fecha border-0" value="{{$solicitud->FechaUltimoEstado}}" disabled>
+                    </div>
+                    <div class="form-group col col-sm-6 flex-column d-flex py-3">
+                            <label class="form-control-label px-3 py-2">Estado</label>
+                            <input type="text" id="estado" class="estado border-0" value='{{$solicitud->UltimoEstado}}'disabled>
+                    </div>
                 </div>
                
                 {{-- nombres y apellidos --}}
-                    <div class="row justify-content-between text-left">
-                            <p class=" "><span class="text-secondary fs-5">Solicitante: </span> {{$solicitud->UsuarioEstudiante}}</p>  
+                    <div class="row justify-content-between text-left cell">
+                        <div class="form-group col col-sm-12 flex-column d-flex py-3">
+                            <label class="form-control-label px-3 py-2">Solicitante</label>
+                            <input type="text" id="nombre " class="nombre border-0" name="nombre" disabled>
+                             <input type="text" id="nombre " class="nombre border-0" name="nombre" value='{{$solicitud->UsuarioEstudiante}}' disabled> 
+                        </div>
+                    </div>
+                    {{-- apellido --}}
+                    <div class="row justify-content-between text-left cell">
+                        <div class="form-group col col-6 flex-column d-flex py-3">
+                        <div class="form-group col col-sm-6 flex-column d-flex py-3">
+                            <label class="form-control-label px-3 py-2">Apellidos</label>
+                            <input type="text" id="apellido" class="apellido border-0" name="apellido" disabled>
+                             <input type="text" id="apellido" class="apellido border-0" name="apellido" value='{{$solicitud->UsuarioEstudiante}}' disabled> 
+                        </div>
                     </div>
                     {{-- legajo --}}
-                    <div class="row justify-content-between text-left ">
-                        <p class=" "><span class="text-secondary fs-5">Legajo: </span> {{$solicitud->Legajo}}</p>  
+                    <div class="row justify-content-between text-left cell">
+                        <div class="form-group col col-sm-6 flex-column d-flex py-3">
+                            <label class="form-control-label px-3 py-2">Legajo</label>
+                            <input type="text" id="legajo" class="legajo border-0" name='legajo' disabled>
+                            <input type="text" id="legajo" class="legajo border-0" name='legajo' value='{{$solicitud->Legajo}}' disabled>
+                        </div>
                     </div>
                    {{-- unidad academica --}}
-                    <div class="row justify-content-between text-left">
-                        <p class=" "><span class="text-secondary fs-5">Unidad Académica: </span> {{$solicitud->UnidadAcademica}}</p>      
+                    <div class="row justify-content-between text-left cell">
+                        <div class="form-group col col-12 flex-column d-flex py-3">
+                            <label class="form-control-label px-3 py-2">Unidad Académica</label>
+                            <input type="text" id="unidad" class="unidad border-0" name="unidadAcademica" disabled>
+                             <input type="text" id="unidad" class="unidad border-0" name="unidadAcademica" value='{{$solicitud->UnidadAcademica}}' disabled> 
+                        </div>
+                        <div class="form-group col col-6 flex-column d-flex py-3">
+                            <label class="form-control-label px-3 py-2">Legajo</label>
+                            <input type="text" id="legajo" class="legajo border-0" name='legajo' disabled>
+                            {{-- <input type="text" id="legajo" class="legajo border-0" name='legajo' value='{{$solicitud->legajo}}' disabled> --}}
+                        </div>
+
                     </div>
                     {{-- carrera --}}
-                    <div class="row justify-content-between text-left ">
-                        <p class=" "><span class="text-secondary fs-5">Carrera: </span> {{$solicitud->Carrera}}</p>      
+                    <div class="row justify-content-between text-left cell">
+                        <div class="form-group col col-12 flex-column d-flex py-3">
+                            <label class="form-control-label px-3 py-2">Carrera</label>
+                            <input type="text" id="carrera" class="carrera border-0" name='carrera' disabled>
+                             <input type="text" id="carrera" class="carrera border-0" name='carrera' value='{{$solicitud->Carrera}}' disabled> 
+                        </div>
                     </div>
                     {{-- universidad de destino --}}
-                    <div class="row justify-content-center text-left">
-                        <p class=" "><span class="text-secondary fs-5">Institución Educativa de Destino: </span> {{$solicitud->UniversidadDestino}}</p>      
+                    <div class="row justify-content-between text-left">
+                        <div class="form-group col col-12 flex-column d-flex py-3">
+                            <label class="form-control-label px-3 py-2">Institución Educativa de Destino</label>
+                            <input type="text" id="destino" class="destino border-0" name='universidadDestino' disabled>
+                             <input type="text" id="destino" class="destino border-0" name='universidadDestino' value="{{$solicitud->UniversidadDestino}}" disabled>
+                        </div>
                     </div>
             </div>            
         </div>
--- resources/views/usuario/registro.blade.php
+++ resources/views/usuario/registro.blade.php
@@ -19,10 +19,10 @@
     {{-- inicio de registro de usuario --}}
     <div class="container-fluid mb-5 p-1 mx-auto">
         <div class="row d-flex justify-content-center">
-            <div class="col-xl-7 col-lg-8 col-md-9 col-11">
+            <div class="col-xl-6 col-lg-6 col-md-12 col-12">
                 <div class="card card-form">
-                    <div class="tittle border-bottom p-2">
-                        <h2 class="text-center fw-bold">Registro de Usuario</h2>
+                    <div class="row tittle border-bottom p-2">
+                        <h2 class="col text-center fw-bold">Registro de Usuario</h2>
                     </div>
                     <form class="">
                         @csrf
@@ -38,38 +38,38 @@
                         </div>
                         @endif
                         <div class="row justify-content-between text-left">
-                            <div class="form-group col-sm-6 flex-column d-flex py-3">
+                            <div class="form-group col col col-sm-6 flex-column d-flex py-3">
                                 <label class="form-control-label px-3 py-2">Nombres</label>
                                 <input class="border-0 cell" type="text" id="nombre" name="nombre" placeholder="Ingrese todos sus nombres">
                             </div>
-                            <div class="form-group col-sm-6 flex-column d-flex py-3">
+                            <div class="form-group col col-sm-6 flex-column d-flex py-3">
                                 <label class="form-control-label px-3 py-2">Apellidos</label>
                                 <input class="border-0 cell" type="text" id="apellido" name="apellido" placeholder="Ingrese todos sus apellidos">
                             </div>
                         </div>
                         <div class="row justify-content-between text-left">
-                            <div class="form-group col-sm-6 flex-column d-flex py-3">
+                            <div class="form-group col col-sm-6 flex-column d-flex py-3">
                                 <label class="form-control-label px-3 py-2">Email</label>
                                 <input class="border-0 cell" type="text" id="email" name="email" placeholder="Ingrese su dirección de email">
                             </div>
-                            <div class="form-group col-sm-6 flex-column d-flex py-3">
+                            <div class="form-group col col-sm-6 flex-column d-flex py-3">
                                 <label class="form-control-label px-3 py-2">Número de Documento</label>
                                 <input class="border-0 cell" type="text" id="dni" name="dni" placeholder="Ingrese su numero de DNI">
                             </div>
                         </div>
                         <div class="row justify-content-between text-left">
-                            <div class="form-group col-sm-6 flex-column d-flex py-3">
+                            <div class="form-group col col-sm-6 flex-column d-flex py-3">
                                 <label class="form-control-label px-3 py-2">Contraseña</label>
                                 <input class="border-0 cell" type="text" id="email" name="email" placeholder="Ingrese su dirección de email">
                             </div>
-                            <div class="form-group col-sm-6 flex-column d-flex py-3">
+                            <div class="form-group col col-sm-6 flex-column d-flex py-3">
                                 <label class="form-control-label px-3 py-2">Verificar Contraseña</label>
                                 <input class="border-0 cell" type="text" id="dni" name="dni" placeholder="Ingrese su numero de DNI">
                             </div>
                         </div>
 
                         <div class="row justify-content-center text-center py-4">
-                            <div class="form-group col-sm-6">
+                            <div class="form-group col col-sm-6">
                                 <button id="boton" name="boton" type="submit" class="btn-block w-100 p-1 rounded-2">Registrarse</button>
                             </div>
                         </div>
@@ -78,38 +78,6 @@
             </div>
         </div>
     </div>
-    {{-- inicio de registro de usuario --}}
-    {{-- formulario de login
-    <div class="container-fluid px-1 py-5 mx-auto">
-        <div class="row d-flex justify-content-center">
-            <div class="col-xl-7 col-lg-8 col-md-9 col-11">
-                <div class="card card-form">
-                    <form class="">
-                        <div class="row justify-content-between text-left">
-                            <div class="form-group col-12 flex-column d-flex py-3">
-                                <label class="form-control-label px-3 py-2">Email</label>
-                                <input class="border-0 cell" type="text" id="email" name="email" placeholder="Ingrese su email">
-                            </div>
-                        </div>
-                        <div class="row justify-content-between text-left">
-                            <div class="form-group col-12 flex-column d-flex py-3">
-                                <label class="form-control-label px-3 py-2">Contraseña</label>
-                                <input class="border-0 cell" type="text" id="password" name="password" placeholder="Ingrese su contraseña">
-                            </div>
-                        </div>
-                        <div class="row justify-content-between text-left py-3">
-                            <p><a href="">¿Olvido su contraseña?</a></p>
-                        </div>
-                        <div class="row justify-content-center text-center py-4">
-                            <div class="form-group col-sm-6">
-                                <button id="boton" name="boton" type="submit" class="btn-block w-100 p-1 rounded-2">Ingresar</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div> --}}
 </main> <!-- Fin main cuerpo -->
 
 @endsection
