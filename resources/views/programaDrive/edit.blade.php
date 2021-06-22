1d51ac45b0eb20f5fc9e9e889ff92044c4277725
laragalazdg <galazlv.nqn@gmail.com>
2021-06-22 20:29:18 -0300

    modificaciones sobre formularios

--- resources/views/programaDrive/edit.blade.php
+++ resources/views/programaDrive/edit.blade.php
@@ -20,19 +20,19 @@
                         @csrf
                         <div class="row justify-content-between text-left">
                             <div class="form-group col col col-sm-6 flex-column d-flex py-3">
-                                <label class="form-control-label px-3 py-2">Nombres Materia</label>
+                                <label class="form-control-label py-2">Nombres Materia</label>
                                 <input class="border-0 cell" type="text" id="nombre" name="nombre" value="{{ old('nombre', $programa->nombre_programa) }}">
                             </div>
                             @method('put')
                             <div class="form-group col col-sm-6 flex-column d-flex py-3">
-                                <label class="form-control-label px-3 py-2">Numero Materia</label>
+                                <label class="form-control-label py-2">Numero Materia</label>
                                 <input class="border-0 cell" type="number" id="numero" name="numero" value="{{ old('numero', $programa->numero_programa) }}">
                             </div>
                         </div>
                      
                         <div class="row justify-content-between text-left">
                             <div class="form-group col col-12 flex-column d-flex py-3">
-                                <label class="form-control-label px-3 py-2">Archivo</label>
+                                <label class="form-control-label py-2">Archivo</label>
                                 <input class="border-0 cell" type="file" id="pdfPrograma" name="pdfPrograma" value="{{ old('pdfPrograma') }}">
                                
                             </div>
@@ -40,7 +40,7 @@
                         
                         <div class="row justify-content-center text-center py-4">
                             <div class="form-group col col-sm-6">
-                                <button id="boton" name="boton" type="submit" class="btn-block w-100 p-1 rounded-2">Crear</button>
+                                <button id="boton" name="boton" type="submit" class="botonFormulario">Crear</button>
                             </div>
                         </div>
                     </form>
