01163738c594aaebb32f2e354e9e340f9474e1c9
jonathanvictorrios <yonamixlfr@gmail.com>
2021-06-22 23:35:49 -0300

    Se agrega controlador hojaResumen y vistas correspondientes

--- resources/views/hojaResumen/create.blade.php
+++ resources/views/hojaResumen/create.blade.php
@@ -1,6 +1,5 @@
 <form action="{{ route('hojaResumen.store') }}" method="POST" autocomplete="off" enctype="multipart/form-data">
     @csrf
-    
     <input type="number" id="idSolicitud" name="idSolicitud">
     <input type="submit" value="enviar">
 </form>
