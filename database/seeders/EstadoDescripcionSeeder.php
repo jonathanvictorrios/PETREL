9d17c44abeaf26af8acf20996f7c0f896a477b68
Sebastian Mon <sebamon12@gmail.com>
2021-06-24 20:25:38 -0300

    Seeder EstadosDescripcio, Estado (#28)

---
+++ database/seeders/EstadoDescripcionSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EstadoDescripcionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('estado_descripcion')->insert([
+            'descripcion'=>'Iniciado'
+        ]);
+        DB::table('estado_descripcion')->insert([
+            'descripcion'=>'Asignado'
+        ]);
+        DB::table('estado_descripcion')->insert([
+            'descripcion'=>'Aguarda Firma Dpto Alumno'
+        ]);
+        DB::table('estado_descripcion')->insert([
+            'descripcion'=>'Aguarda Firma Secretaria Academica'
+        ]);
+        DB::table('estado_descripcion')->insert([
+            'descripcion'=>'Terminado'
+        ]);
+
+    }
+}
