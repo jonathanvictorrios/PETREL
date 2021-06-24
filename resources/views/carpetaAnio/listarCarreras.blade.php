ec0618d1b923b71fe7b6e2266fc06372d8c44f8c
wtfranco22 <wtfranco22@hotmail.com>
2021-06-24 04:38:24 -0300

    eliminacion de vistas y cambios y en el filtro de seleccion para crear nuevas carpetas

--- resources/views/carpetaAnio/listarCarreras.blade.php
+++ resources/views/carpetaAnio/listarCarreras.blade.php
@@ -19,9 +19,6 @@
             <tr>
                 <td>{{ $carrera->carrera->carrera }}</td>
                 <td>
-                    <a href="{{ route('carrera.show', $carrera->id_carpeta_carrera) }}">Editar</a>
-                </td>
-                <td>
                     <a href=" {{ route('verProgramas',$carrera->id_carpeta_carrera) }} ">Ver programas</a>
                 </td>
             </tr>
