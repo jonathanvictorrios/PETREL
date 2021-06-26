64ccb96442f32135ac0debb4aa9b655292e0afca
laragalazdg <galazlv.nqn@gmail.com>
2021-06-26 17:13:25 -0300

    modificaciones input file

--- resources/views/programaDrive/create.blade.php
+++ resources/views/programaDrive/create.blade.php
@@ -32,7 +32,7 @@
                         <div class="row justify-content-between text-left">
                             <div class="form-group col col-12 flex-column d-flex py-3">
                                 <label class="form-label py-2">Archivo</label>
-                                <input class="border-0 cell" type="file" id="pdfPrograma" name="pdfPrograma" value="{{ old('pdfPrograma') }}">
+                                <input class="border-0" type="file" id="pdfPrograma" name="pdfPrograma" value="{{ old('pdfPrograma') }}">
 
                             </div>
 
@@ -49,23 +49,5 @@
         </div>
     </div>
 </main> <!-- Fin main cuerpo -->
-
-@endsection
-{{-- @extends('estructura/layout')
-@section('cuerpo')
-@include('estructura/header')
-
-<form action="{{ route('programaDrive.store') }}" method="POST" autocomplete="off" enctype="multipart/form-data">
-    @csrf
-    <h1>{{$carpetaCarrera->carrera->carrera.' '.$carpetaCarrera->carpeta_anio->numero_anio}}</h1>
-    <input type="hidden" id="idCarpetaCarrera" name="idCarpetaCarrera" value="{{ $carpetaCarrera->id_carpeta_carrera }}">
-        Nombre materia:<input type="text" id="nombrePrograma" name="nombrePrograma" value="{{ old('nombrePrograma') }}">
-    <br>
-        Numero materia:<input type="text" id="numeroPrograma" name="numeroPrograma" value="{{ old('numeroPrograma') }}">
-    <br>
-        Archivo:<input type="file" id="pdfPrograma" name="pdfPrograma" value="{{ old('pdfPrograma') }}">
-    <br>
-    <input type="submit" value="enviar">
-</form>
 
 @endsection
