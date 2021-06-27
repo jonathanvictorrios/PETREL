a3511c1303c90cfd079e4ac30d5f6c9dff9f5de8
xTorettox <fg.garciacendra@gmail.com>
2021-06-27 01:38:05 -0300

    modificaciones sobre mailPetrel

--- app/Mail/mailPetrel.php
+++ app/Mail/mailPetrel.php
@@ -6,20 +6,43 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use App\Models\SolicitudCertProg;
+use App\Models\Usuario;
 
 class mailPetrel extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public $subject = "Info sobre tu pedido de Solicitud";
+    public $subject = 'Solicitud de trámite iniciada';
+    public $datosMail;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($id)
     {
-        //
+        // Recupero la solicitud
+        $solicitud = SolicitudCertProg::findOrFail($id);
+
+        // Se genera una nueva instancia de SolicitudCertProg y se cargan los datos:
+        $datosMail = new SolicitudCertProg;
+        $datosMail->idSolicitud = $id;
+
+        $datosMail->Nombre = $solicitud->usuarioEstudiante->nombre;
+        $datosMail->Apellido = $solicitud->usuarioEstudiante->apellido;
+        $datosMail->Legajo = $solicitud->legajo;
+        $datosMail->Carrera = $solicitud->carrera->carrera;
+        $datosMail->UniversidadDestino = $solicitud->universidad_destino;
+
+        // Se genera una nueva instancia de Usuario para obtener su correo electrónico
+        $destinatario = new Usuario;
+        $destinatario = Usuario::findOrFail($solicitud->id_usuario_estudiante);
+        $datosMail->correoUsuario = $destinatario->email;
+        //print($datosMail);
+        //return view('emails/solicitud_iniciada', compact('datosMail'));
+        $this->datosMail = $datosMail;
+        //print($this->datosMail);
     }
 
     /**
