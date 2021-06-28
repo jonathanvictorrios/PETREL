e27957a5d89ff20286190780df10d0f75e4f8193
venserthesojourner <luchoc2812@gmail.com>
2021-06-27 21:42:11 -0300

    21:42 HS

--- app/Http/Requests/StoreRolesRequest.php
+++ app/Http/Requests/StoreRolesRequest.php
@@ -1,9 +1,9 @@
 <?php
-namespace App\Http\Requests;
+namespace App\Http\Requests\Admin;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdatePermissionsRequest extends FormRequest
+class StoreRolesRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
